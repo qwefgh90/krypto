6a4919a6e04fcccf2b6de01a7869598f472f58b5
qwefgh90 <qwefgh90@naver.com>
2023-05-11 21:28:04 +0900

    changes for deploying to OSSRH

--- src/main/java/krypto/Algorithm.java
+++ src/main/java/krypto/Algorithm.java
@@ -2,10 +2,24 @@ package krypto;
 
 public abstract class Algorithm {
 
+    /**
+     * The algorithm kind of this instance
+     */
     protected final Kind kind;
 
+    /**
+     * The seed value for creating a secret key
+     */
     protected final String masterKey;
+
+    /**
+     * A byte array of the masterKey
+     */
     protected final byte[] keyBytes;
+
+    /**
+     * The length of the key for encryption and descryption
+     */
     protected final int keyLength;
 
     public Algorithm(Kind kind, String masterKey, byte[] keyBytes, int keyLength) {
@@ -20,17 +34,22 @@ public abstract class Algorithm {
     }
 
     /**
-     * @param bytes any plain text
-     * @return
+     * Encrypt a plain text.
+     * @param bytes Any plain text
+     * @return A cipher text
      */
     public abstract byte[] encrypt(byte[] bytes);
 
     /**
-     * @param bytes any cipher text which is encrypted with supported algorithms
-     * @return
+     * Decrypt a cipher text.
+     * @param bytes Any cipher text which is encrypted with supported algorithms
+     * @return A decrypted text
      */
     public abstract byte[] decrypt(byte[] bytes);
 
+    /**
+     * This enum refers to a list of algorithms which implementation is provided.
+     */
     public enum Kind {
         ARIA,
         SEED,
